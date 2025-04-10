db6a09f27160a6d3d2344c5feceb4252101d1399
Florian Hahn <icqspezial@web.de>
2025-04-10 23:45:05 +0200

    Add files via upload

---
+++ sodrepoData.js
@@ -0,0 +1,102 @@
+export const sodrepoData = {
+    openDataPortals: [
+          {
+            "name": "DreiD",
+            "nameEn": "DreiD",
+            "mainTopic":"3D-Stadtmodelle",
+            "portalType": "Geo",
+            "portalTypeEn": "Geo",
+            "inFederalState": "Sachsen",
+            "inFederalState_en": "Saxony",
+            "inCountry": "Deutschland",
+            "inCountryEn": "Germany",
+            "isCountry": false,
+            "isFederalState": false,
+            "isCity": false,
+            "isGovernment": false,
+            "isCivilLaw": false,
+            "isOther:":true,
+            "levelInEu":5,
+            "geoBoundariesLevel": null,
+            "geoBoundariesType": "ADM1",
+            "relatedToEu":true,
+            "license": "Offene Lizenz laut Portalangaben",
+            "licenseEn": "Open license according to portal information",
+            "publisher": {
+              "name": "Unbekannt",
+              "nameEn": "Unknown",
+              "contactEmail": null
+            },
+            "url": "https://www.drei-d.sachsen.de/",
+            "subdomain":true,
+            "urlSubdomainIsOpenData": false,
+            "datasetsCount": null,
+            "datasetUploadLastThreeMonths":false,
+            "datasetUpdatedLastThreeMonths":false,
+            "categoriesCount": null,
+            "portalSoftware": null,
+            "openDataRanking":false,
+            "openDataRankingPoints":0,
+            "openDataInventoryRankingPoints": 0,
+            "openDataBarometerPoints": 0,
+            "globalDataBarometerPoints": 0,
+            "oecdOurdataInexPoints": 0,
+            "opendataMaturityStudiePoints": 0,
+            "openDataCityCensusPoints":0,
+            "dataFormats": [],
+            "mostDataFormat": null,
+            "fiveStar": 1,
+            "dcat": {
+                "details": null,
+                "available": false,
+                "url": null,
+                "type": null,
+                "DCAT": true,
+                "DCAT-AP": true,
+                "DCAT-AP_SHACL": false
+            },
+            "ckan":{
+                "details": null,
+                "available": false,
+                "url": null,
+                "type": null
+            },
+            "apiDocumentation": {
+                "details": null,
+                "available": false,
+                "url": null,
+                "type": null
+            },
+            "mqa": {
+                "details": null,
+                "available": false,
+                "url":null,
+                "points": 0
+            },
+            "sparql": {
+                "details": null,
+                "available": false,
+                "url": null,
+                "endpoint": null
+            },
+            "graphql": {
+                "details": null,
+                "available": false,
+                "url": null,
+                "endpoint": null
+            },
+            "metadata":{
+                "details": null,
+                "available": false,
+                "url": null,
+                "endpoint": null,
+                "formats":[]
+            },
+            "additionalProperties": {
+              "notes": "Details zu DreiD sind noch zu validieren",
+              "notesEn": "Details for DreiD are yet to be validated"
+            }
+          }
+    ]
+};
+  
