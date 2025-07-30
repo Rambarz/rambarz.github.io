9fc53fec7f0ed636e220539a8de230ab9abc68d7
Rambarz <icqspezial@web.de>
2025-07-30 10:03:59 +0200

    new major eu version 1.0

--- data/defaultValues.js
+++ data/defaultValues.js
@@ -3,7 +3,7 @@
   export const defaultZoom = 5.5;
   export const defaultEuCountries = [
   "EU", "Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czechia", "Denmark",
-  "Estonia", "Finland", "France", "the Federal Republic of Germany", "Greece", "Hungary", "Ireland", "Italy",
+  "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy",
   "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal",
   "Romania", "Slovakia", "Slovenia", "Spain", "Sweden"
 ];
